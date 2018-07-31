7bbb7ed8dd238d1885b844ea23c563f96c2ce86f
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-08-01 00:09:40 +0200

    Leaderboard

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { Avatar, Badge, List } from 'antd';
 
 class Leaderboard extends Component {
   render() {
+    const { users } = this.props;
+    const data = _.chain(users.users)
+      .map(user => ({
+        ...user,
+        rank: Object.keys(_.get(user, 'answers', {})).length + _.get(user, 'questions', []).length,
+      }))
+      .sortBy(user => -1 * user.rank)
+      .value();
 
     return (
-      <p>TODO: Leaderboard</p>
+      <List
+        itemLayout="horizontal"
+        dataSource={ data }
+        renderItem={ user => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={ <Avatar src={ user.avatarURL } /> }
+              title={ user.name }
+              description={
+                `${ Object.keys(_.get(user, 'answers', {})).length } created
+                /
+                ${ _.get(user, 'questions', []).length } answered`
+              }
+            />
+            <Badge count={ user.rank } />
+          </List.Item>
+        )}
+      />
     );
   }
 }
 
 Leaderboard.propTypes = {
-
+  users: PropTypes.object.isRequired,
 };
 
 export default connect((state) => ({
-  
+  users: state.users,
 }))(Leaderboard);
